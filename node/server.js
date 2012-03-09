c5211b94f5a138b5ae749107a26e7bfefe688679
J. Random Hacker (duk3luk3) <l.erlacher+github@googlemail.com>
2012-03-09 16:33:57 +0100

    Added more code and stubs.

--- node/server.js
+++ node/server.js
@@ -11,8 +11,10 @@
 
 var sys = require("sys"),  
     http = require("http"),  
-    url = require("url"),  
-    path = require("path"),  
+    url = require("url"),
+    qry = require("querystring"),
+    path = require("path"),
+    sock = require("socket.io"),
     fs = require("fs");
 
 /*
@@ -24,7 +26,13 @@ var configFile = path.join(process.cwd(), "config.json");
 
 //sane defaults
 var config = {
-  database: 'jsdb.sqlite'
+  db_engine: 'sqlite',
+  db_sqlite_file: 'jsdb.sqlite',
+  
+  html_dir: 'html'
+  html_files: {
+    'index.html'
+  }
 }
 
 //look for config file, load if present
@@ -38,3 +46,66 @@ path.exists(configFile, function(exists) {
 	}
     });  
 });
+
+/*
+ * create server
+ */
+ 
+var app = http.createServer(httpHandler);
+var io  = sock.listen(app);
+
+/*
+ *  simple http handler that serves html files containing socket.io client
+ */
+
+function httpHandler(req, res) {
+  fs.readFile(serveFile('index.html'),
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+/*
+ *  socket.io handler. This is where all the magic happens.
+ */
+ 
+ 
+//stubs
+
+var io_chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
+  });
+
+var io_news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
+
+
+/*
+ * functions
+ */
+ 
+function serveFile(file) {
+  if (config.html_files.indexOf(file) < 0) {
+    file = config.html_files[0];
+  }
+    
+  return path.join(process.cwd(), config.html_dir, file);  
+}
