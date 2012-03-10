f838c0b5468e0c18661cb4ebb4d2b917daa57b0b
J. Random Hacker (duk3luk3) <l.erlacher+github@googlemail.com>
2012-03-10 02:05:02 +0100

    some real network code , and client html

--- node/server.js
+++ node/server.js
@@ -9,13 +9,13 @@
  * imports
  */
 
-var sys = require("sys"),  
+var //sys = require("sys"),  
     http = require("http"),  
     url = require("url"),
-    qry = require("querystring"),
     path = require("path"),
-    sock = require("socket.io"),
-    fs = require("fs");
+    fs = require("fs"),
+    sqlite3 = require('sqlite3').verbose(),
+    sock = require('socket.io');
 
 /*
  * load config
@@ -29,11 +29,11 @@ var config = {
   db_engine: 'sqlite',
   db_sqlite_file: 'jsdb.sqlite',
   
-  html_dir: 'html'
-  html_files: {
+  html_dir: '../asset/html',
+  html_files: [
     'index.html'
-  }
-}
+  ]
+};
 
 //look for config file, load if present
 path.exists(configFile, function(exists) {
@@ -44,8 +44,23 @@ path.exists(configFile, function(exists) {
 			config = JSON.parse(data);			
 		});
 	}
-    });  
-});
+    });
+
+/*
+ * create state
+ */
+ 
+var state = {
+  users: [],
+  sessions [],
+  
+  
+  userExists: function(usrname) {
+    if (users.indexOf(
+  },
+};
+ 
+//db magic happens here
 
 /*
  * create server
@@ -53,6 +68,8 @@ path.exists(configFile, function(exists) {
  
 var app = http.createServer(httpHandler);
 var io  = sock.listen(app);
+
+app.listen(8088);
 
 /*
  *  simple http handler that serves html files containing socket.io client
@@ -73,13 +90,62 @@ function httpHandler(req, res) {
 
 /*
  *  socket.io handler. This is where all the magic happens.
+ *
+ *  We need channels to
+ *  - Establish a session (register/logging in)
+ *    - register: client sends nick and password, receives session id
+ *  - Grab non-game data
+ *    - ?
+ *  - Start a game, submit and receive moves
+ *    - get data for lobby
+ *    - start a new game
  */
  
  
-//stubs
+//session io
+var io_session = io
+  .of('/session')
+  .on('connection', function (socket) {
+    
+    socket.on('register', function(from, msg) {
+    
+      if (state.userExists(msg.nickname)) {
+        socket.emit('register',{ result: 'fail', reason: 'Nick already registered'} );
+      }
+      else {
+        
+        var user = state.userAdd(msg.nickname, msg.password);
+        var session = state.sessionCreate(user);
+        
+        socket.emit('register', { result: 'success', sessid: session.sessid } );
+      }
+    });
+      
+    socket.on('login', function(from, msg) {
+    
+      if (state.userExists(msg.nickname)) {
+        
+        var user = state.users['msg.nickname'];
+        
+        if (state.userLogin(msg.password)) {
+          var session = state.sessionCreate(user);
+          
+          socket.emit('login', { result: 'success', sessid: session.sessid } );
+        
+        }
+        else {
+          socket.emit('login',{ result: 'fail', reason: 'Authentication failure'} );
+        }
+      }
+      else {
+        socket.emit('login',{ result: 'fail', reason: 'Authentication failure'} );   
+      }
+    });
+  });
 
-var io_chat = io
-  .of('/chat')
+
+var io_data = io
+  .of('/data')
   .on('connection', function (socket) {
     socket.emit('a message', {
         that: 'only'
@@ -91,16 +157,26 @@ var io_chat = io
     });
   });
 
-var io_news = io
-  .of('/news')
+var io_game = io
+  .of('/game')
   .on('connection', function (socket) {
     socket.emit('item', { news: 'item' });
+  });
+  
+var io_hello = io
+  .of('/hello')
+  .on('connection', function (socket) {
+    socket.emit('hello', { news: 'item' });
   });
 
 
 /*
  * functions
  */
+ 
+function queryParams(req) {
+  return url.parse(req.url, true).query;
+}
  
 function serveFile(file) {
   if (config.html_files.indexOf(file) < 0) {
